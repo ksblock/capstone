47c4834ed0a517ea02d5759eb549d87b4342aad2
kimsihwan <ksshhses@ajou.ac.kr>
2022-05-07 18:56:34 +0900

    add reply

--- routes/board.js
+++ routes/board.js
@@ -23,19 +23,37 @@ router.get('/list', function(req, res) {
 })
 
 router.get('/detail/:id', function(req, res) {
-    console.log(req.params.id);
+
     const param = [req.params.id];
-    //var sql = 'select board.board_id, board.title, users.nickname, board.date from users, board where board.user_id = users.user_id;';
-    var sql = "select board.title, board.content, board.large, board.small, board.event, users.id from users, board where board.board_id=? and board.user_id=users.user_id;"
+    
+    var sql = "select board.title, board.content, board.large, board.small, board.event, users.nickname from users, board where board.board_id=? and board.user_id=users.user_id;"
+    
     conn.query(sql, param, (err, result) => {
         console.log(result);
-        console.log(req.id);
+        console.log(req.params.id);
 
         var json = JSON.stringify(result);
 
         console.log(json);
         console.log(typeof(json));
         
+        res.send(json);
+        //res.send(path.join(__dirname, "../public/board", "board.html"), {"result": json});
+    })
+    
+}) 
+
+router.get('/detail/:id/reply', (req, res) => {
+    const param = [req.params.id];
+
+    var sql_reply = "select reply.content, reply.date, users.nickname from users, reply where reply.board_id=? and users.user_id=reply.user_id;"
+    
+    conn.query(sql_reply, param, (err, result) => {
+        var json = JSON.stringify(result);
+
+        console.log(json);
+        console.log(typeof(json));
+
         res.send(json);
         //res.send(path.join(__dirname, "../public/board", "board.html"), {"result": json});
     })
@@ -56,6 +74,20 @@ router.post('/write', (req, res) => {
             return res.redirect('/board/write');
         }
         res.redirect('/board/list');
+    })
+})
+
+router.post('/detail/:id/writeReply', (req, res) => {
+    const param = [req.params.id, req.body.user_id, req.body.content];
+    var sql = "insert into reply values(NULL, now(), ?, ?, ?);";
+
+    conn.query(sql, param, (err, result) => {
+        if(err)
+        {
+            console.log(err);
+            return res.redirect('/board/list');
+        }
+        res.redirect('/board/detail/' + req.params.id);
     })
 })
 
