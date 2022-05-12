5789e69fbd1a9c1e6d998b17b5b85c7acc76d78d
LEEJUNHA <hiru0926@git.ajou.ac.kr>
2022-05-12 17:01:32 +0900

    Add delete board

--- routes/board.js
+++ routes/board.js
@@ -1,94 +1,105 @@
-const express = require('express');
-const { redirect } = require('express/lib/response');
-const { appendFile } = require('fs');
-const path = require('path');
-const conn = require('../config/db_config');
+const express = require("express");
+const { redirect } = require("express/lib/response");
+const { appendFile } = require("fs");
+const path = require("path");
+const conn = require("../config/db_config");
 
 const router = express.Router();
 
-router.get('/list', function(req, res) {
-    console.log("test");
-    var sql = 'select board.board_id, board.title, users.nickname, board.date from users, board where board.user_id = users.user_id;';
-    conn.query(sql, [], (err, result) => {
-        console.log("success");
+// 게시글 목록
+router.get("/list", function (req, res) {
+  console.log("test");
+  var sql =
+    "select board.board_id, board.title, users.nickname, board.date from users, board where board.user_id = users.user_id;";
+  conn.query(sql, [], (err, result) => {
+    console.log("success");
 
-        var json = JSON.stringify(result);
+    res.send(result);
+    //res.send(path.join(__dirname, "../public/board", "board.html"), {"result": json});
+  });
+});
 
-        console.log(json);
-        console.log(typeof(json));
-        
-        res.send(json);
-        //res.send(path.join(__dirname, "../public/board", "board.html"), {"result": json});
-    })
-})
+// 개별 게시글 조회
+router.get("/detail/:id", function (req, res) {
+  const param = [req.params.id];
 
-router.get('/detail/:id', function(req, res) {
+  var sql =
+    "select board.title, board.content, board.large, board.small, board.event, users.nickname from users, board where board.board_id=? and board.user_id=users.user_id;";
 
-    const param = [req.params.id];
-    
-    var sql = "select board.title, board.content, board.large, board.small, board.event, users.nickname from users, board where board.board_id=? and board.user_id=users.user_id;"
-    
-    conn.query(sql, param, (err, result) => {
-        console.log(result);
-        console.log(req.params.id);
+  conn.query(sql, param, (err, result) => {
+    console.log(result);
 
-        var json = JSON.stringify(result);
+    res.send(result);
+    //res.send(path.join(__dirname, "../public/board", "board.html"), {"result": json});
+  });
+});
 
-        console.log(json);
-        console.log(typeof(json));
-        
-        res.send(json);
-        //res.send(path.join(__dirname, "../public/board", "board.html"), {"result": json});
-    })
-    
-}) 
+// 게시글에 달린 댓글 조회
+router.get("/detail/:id/reply", (req, res) => {
+  const param = [req.params.id];
 
-router.get('/detail/:id/reply', (req, res) => {
-    const param = [req.params.id];
+  var sql_reply =
+    "select reply.content, reply.date, users.nickname from users, reply where reply.board_id=? and users.user_id=reply.user_id;";
 
-    var sql_reply = "select reply.content, reply.date, users.nickname from users, reply where reply.board_id=? and users.user_id=reply.user_id;"
-    
-    conn.query(sql_reply, param, (err, result) => {
-        var json = JSON.stringify(result);
+  conn.query(sql_reply, param, (err, result) => {
+    console.log(result);
 
-        console.log(json);
-        console.log(typeof(json));
+    res.send(result);
+    //res.send(path.join(__dirname, "../public/board", "board.html"), {"result": json});
+  });
+});
 
-        res.send(json);
-        //res.send(path.join(__dirname, "../public/board", "board.html"), {"result": json});
-    })
-})
+router.get("/write", (req, res) => {
+  res.send("write board");
+});
 
-router.get('/write', (req, res) => {
-    res.send("write board")
-})
+// 게시글 작성
+router.post("/write", (req, res) => {
+  const params = [
+    req.body.user_id,
+    req.body.title,
+    req.body.content,
+    req.body.large,
+    req.body.small,
+    req.body.event,
+  ];
+  //var sql = "insert into board (date, title, content) values(now(), ?, ?);";
+  var sql = "insert into board values(NULL, now(), ?, ?, ?, ?, ?, ?);";
+  conn.query(sql, params, (err, result) => {
+    if (err) {
+      console.log(err);
+    }
+    res.send({ success: true });
+  });
+});
 
-router.post('/write', (req, res) => {
-    const params = [req.body.user_id, req.body.title, req.body.content, req.body.large, req.body.small, req.body.event];
-    var sql = "insert into board values(NULL, now(), ?, ?, ?, ?, ?, ?);"
-    
-    conn.query(sql, params, (err, result) => {
-        if(err)
-        {
-            console.log(err);
-            return res.redirect('/board/write');
-        }
-        res.redirect('/board/list');
-    })
-})
+// 해당하는 게시글의 댓글 작성
+router.post("/detail/:id/writeReply", (req, res) => {
+  const param = [req.params.id, req.body.user_id, req.body.content];
+  //DB column 순서 주의
+  var sql = "insert into reply values(NULL, ?, ?, ?, now());";
 
-router.post('/detail/:id/writeReply', (req, res) => {
-    const param = [req.params.id, req.body.user_id, req.body.content];
-    var sql = "insert into reply values(NULL, now(), ?, ?, ?);";
+  conn.query(sql, param, (err, result) => {
+    if (err) {
+      console.log(err);
+    }
+    res.send({ success: true });
+  });
+});
 
-    conn.query(sql, param, (err, result) => {
-        if(err)
-        {
-            console.log(err);
-            return res.redirect('/board/list');
-        }
-        res.redirect('/board/detail/' + req.params.id);
-    })
-})
+// 게시글 삭제
+router.delete("/detail/:id", (req, res) => {
+  const param = [req.params.id, req.body.user_id];
+  // 게시글 삭제 요청을 보낸 user_id와 게시글을 작성한 user_id가 같을 경우 삭제
+  // 프론트에서 처리 어떻게?
+  var sql = "DELETE FROM board WHERE board.board_id = ? and board.user_id = ?;";
+
+  conn.query(sql, param, (err, result) => {
+    if (err) {
+      console.log(err);
+    }
+    res.send({ success: true, result: result });
+  });
+});
 
 module.exports = router;
